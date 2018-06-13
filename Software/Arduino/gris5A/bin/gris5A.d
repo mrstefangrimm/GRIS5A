639149eb2fb61b6327f7a567219cf9285398594b
= <=>
2018-06-13 22:34:23 +0200

    Added the motion phantom software

---
+++ Software/Arduino/gris5A/bin/gris5A.d
@@ -0,0 +1,18 @@
+gris5A.o: gris5A.ino \
+ C:/Users/STEFAN~1/DOCUME~1/Arduino/libraries/qpn_avr/qpn.h \
+ C:/PROGRA~2/Arduino/hardware/arduino/avr/cores/arduino/Arduino.h \
+ C:/PROGRA~2/Arduino/hardware/arduino/avr/cores/arduino/binary.h \
+ C:/PROGRA~2/Arduino/hardware/arduino/avr/cores/arduino/WCharacter.h \
+ C:/PROGRA~2/Arduino/hardware/arduino/avr/cores/arduino/WString.h \
+ C:/PROGRA~2/Arduino/hardware/arduino/avr/cores/arduino/HardwareSerial.h \
+ C:/PROGRA~2/Arduino/hardware/arduino/avr/cores/arduino/Stream.h \
+ C:/PROGRA~2/Arduino/hardware/arduino/avr/cores/arduino/Print.h \
+ C:/PROGRA~2/Arduino/hardware/arduino/avr/cores/arduino/Printable.h \
+ C:/PROGRA~2/Arduino/hardware/arduino/avr/cores/arduino/USBAPI.h \
+ C:/PROGRA~2/Arduino/hardware/arduino/avr/cores/arduino/Arduino.h \
+ C:/PROGRA~2/Arduino/hardware/arduino/avr/cores/../variants/standard/pins_arduino.h \
+ C:/Users/STEFAN~1/DOCUME~1/Arduino/libraries/gris5Alib/_Adafruit_PWMServoDriver.h \
+ C:/Users/STEFAN~1/DOCUME~1/Arduino/libraries/gris5Alib/_Wire.h \
+ C:/PROGRA~2/Arduino/hardware/arduino/avr/cores/arduino/Stream.h \
+ C:/Users/STEFAN~1/DOCUME~1/Arduino/libraries/prfServo/prfServo.h \
+ C:/Users/STEFAN~1/DOCUME~1/Arduino/libraries/prfServo/PolynomialRegression.h
