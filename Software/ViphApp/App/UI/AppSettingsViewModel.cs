740c0d64f229459b8f1650ab8e19a67569c9a733
mrstefangrimm <motionsystems@grimm.com>
2020-05-11 18:48:02 +0200

    Update license statement.

--- Software/ViphApp/App/UI/AppSettingsViewModel.cs
+++ Software/ViphApp/App/UI/AppSettingsViewModel.cs
@@ -12,7 +12,7 @@
  * GNU Lesser General Public License for more details.
 
  * You should have received a copy of the GNU Lesser General Public License
- * along with the VirtualGris05 software.  If not, see
+ * along with the ViphApp software.  If not, see
  * <http://www.gnu.org/licenses/>.
  */
 
